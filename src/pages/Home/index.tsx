a4bcc2bd65ae6cd6d95bc2e753c82af04aacc57c
mucha17 <46112158+mucha17@users.noreply.github.com>
2021-01-10 18:45:49 +0100

    Remove unnecessery code

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -6,7 +6,24 @@ const HomePage = (): JSX.Element => {
 		localStorage.getItem("selectedLanguage")?.toString() ?? "pl";
 	strings.setLanguage(selectedLanguage ?? "pl");
 
-	return <div>Strona glowna</div>;
+	return (
+		<div>
+			<h1>View Video</h1>
+			<video width="400" controls>
+				<source
+					src={
+						"http://localhost:8080/api/videos/5ff7b64b8438804e301f9af8"
+					}
+				/>
+			</video>
+			<h1>View Image</h1>
+			<img
+				src={
+					"http://localhost:8080/api/images/5ff7c6403c807b6f48590c1f"
+				}
+			/>
+		</div>
+	);
 };
 
 export default HomePage;
