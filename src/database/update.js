da364b85da5a2135aa903c67e71d33480b4c45c9
AlbertZawadzki <r3vis3dsmil3@gmail.com>
2021-02-01 16:08:24 +0100

    int

--- src/database/update.js
+++ src/database/update.js
@@ -2,19 +2,15 @@ import {addNotification} from "../redux/actions/notification"
 import store from "../redux/store"
 import * as CFG from "./config"
 
+import axios from "axios";
+
 const update = async (url, action, form) => {
     url = CFG.BACKEND_URL + url
     store.dispatch(
         addNotification({status: "processing", title: "Updating..."}),
     )
 
-    return await fetch(url, {method: "POST", body: form})
-        .then(async (res) => {
-            return {
-                data: await res.json(),
-                status: res.status,
-            }
-        })
+    return axios.put(url, {form})
         .then((info) => {
             return CFG.handleStatus(info, action)
         })
